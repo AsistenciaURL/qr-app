ec8c5fa34c8041d17621274e3d3296c5b0ed7504
unknown <tatobig@gmail.com>
2022-11-06 20:19:17 -0600

    Added build condig, added assistance procedure

--- screens/Scanner.tsx
+++ screens/Scanner.tsx
@@ -9,10 +9,13 @@ import {
 } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import * as Application from "expo-application";
+import * as Location from "expo-location";
 
 const Scanner = () => {
   const [hasPermission, setHasPermission] = useState<null | boolean>(null);
   const [scanned, setScanned] = useState(false);
+  const [location, setLocation] = useState<any>(null);
+  const [errorMsg, setErrorMsg] = useState<any>(null);
 
   useEffect(() => {
     (async () => {
@@ -21,24 +24,87 @@ const Scanner = () => {
     })();
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   const handleBarCodeScanned = async ({ data }: { data: string }) => {
     setScanned(true);
+    alert('Scanned')
 
-    let uniqueId
+    let uniqueId;
     if (Platform.OS === "android") {
       uniqueId = Application.androidId;
     }
     if (Platform.OS === "ios") {
       uniqueId = await Application.getIosIdForVendorAsync();
     }
-    alert(`DATA: ${data} ID: ${uniqueId}`);
-    // const supported = await Linking.canOpenURL(data);
 
-    // if (supported) {
-    //   await Linking.openURL(data);
-    // } else {
-    //   alert(`Don't know how to open this URL: ${data}`);
-    // }
+    const regex = /http:\/\/localhost:3000\/assistance\/[0-9]+/gm;
+    const validUrl = regex.exec(data);
+    if (validUrl && validUrl.length === 1) {
+      const qrId = data.replace("http://localhost:3000/assistance/", "");
+
+      const supported = await Linking.canOpenURL(data);
+
+      if (supported) {
+        const registerDevice = await fetch(`http://192.168.0.3:8000/register/`, {
+          method: "POST",
+          body: JSON.stringify({
+            id: uniqueId,
+          }),
+          headers: { "Content-Type": "application/json" },
+        });
+        const registerDeviceData = await registerDevice.json();
+
+        if (registerDeviceData.status === "success") {
+          const response = await fetch(
+            `http://192.168.0.3:8000/validate/${qrId}`,
+            {
+              method: "POST",
+              body: JSON.stringify({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                deviceId: uniqueId,
+              }),
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+          const resData = await response.json();
+          alert(
+            `DATA: ${data} ID: ${uniqueId} Locación ${JSON.stringify(
+              location
+            )} ${JSON.stringify(resData)}`
+          );
+          if (resData.status === "success") {
+            await Linking.openURL(
+              `http://192.168.0.3:3000/assistance/${resData.data}/${uniqueId}`
+            );
+          }
+        }
+      } else {
+        alert(`Don't know how to open this URL: ${data}`);
+      }
+    } else {
+      alert("El QR escaneado no pertenece al sistema.");
+    }
   };
 
   if (hasPermission === null) {
